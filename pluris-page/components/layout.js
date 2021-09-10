cd5272a503230000e14373a4ce2c81481eadf4b2
Amanda Carneiro <amandabcarneiro9@gmail.com>
2021-09-10 09:43:25 +0200

    home started

--- pluris-page/components/layout.js
+++ pluris-page/components/layout.js
@@ -2,15 +2,33 @@ import Head from 'next/head'
 import Link from 'next/link'
 import Header from './Header'
 import styles from './layout.module.scss'
+import Image from 'next/image'
 
-export default function Layout({ children }) {
+const name = 'home foto'
+export default function Layout({ home, children, siteTitle }) {
   return (
     <div className={styles.container}>
       <Head>
         {/* <link rel="icon" href="/favicon.ico" /> */}
         <meta name="Pluris engenharia" content=" Pluris serviços de Engenharia" />
+
+        <title>{siteTitle}</title>
       </Head>
       <Header />
+      {home ? (
+        <>
+          <div>
+            <Image priority src="/images/home.jpg" height={650} width={1300} alt={name} />
+          </div>{' '}
+          <h1 className={styles.tituloHome}>Qualidade e Confiança em Primeiro Lugar</h1>
+          <p className={styles.paragrafoHome}>
+            Na Pluris Engenharia você encontra profissionais especializados que analisam as melhores condições para
+            realizar os sonhos dos seus clientes.{' '}
+          </p>
+        </>
+      ) : (
+        ''
+      )}
     </div>
   )
 }
--- pluris-page/pages/index.js
+++ pluris-page/pages/index.js
@@ -1,8 +1,18 @@
 import Head from 'next/head'
 import Layout from '../components/layout'
-
+import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
+const name = 'home foto'
+const siteTitle = 'Início'
+
 export default function Home() {
-  return <Layout></Layout>
+  return (
+    <Layout home>
+      <Head>
+        <title>{siteTitle}</title>
+      </Head>
+      <Image priority src="/images/home.jpg" height={80} width={80} alt={name} />
+    </Layout>
+  )
 }
