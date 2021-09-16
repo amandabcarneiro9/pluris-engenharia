9e94222ba2cb2c34eb98b688d04086de1dec855c
Amanda Carneiro <amandabcarneiro9@gmail.com>
2021-09-16 11:10:40 +0200

    advices from UX

--- pluris-page/components/About.jsx
+++ pluris-page/components/About.jsx
@@ -1,49 +1,64 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import Style from './About.module.scss'
-import utilStyles from '../styles/util.module.scss'
+import Util from '../styles/util.module.scss'
 
 export default function About() {
   return (
-    <div className={Style.container}>
-      <div className={Style.values}>
-        <div className={Style.value}>
-          <div className={Style.icon}>
-            <FontAwesomeIcon icon={['fas', 'award']} />
+    <>
+      <div className={Style.container}>
+        <div className={Style.values}>
+          <div className={Style.value}>
+            <div className={Style.icon}>
+              <FontAwesomeIcon icon={['fas', 'award']} />
+            </div>
+            <span className={Style.valueSpan}>
+              Qualidade em todos os processos. Desde o projeto até o seu desenvolvimento.{' '}
+            </span>
           </div>
-          <span>Qualidade em todos os processos. Desde o projeto até o seu desenvolvimento. </span>
+
+          <div className={Style.value}>
+            <div className={Style.icon}>
+              <FontAwesomeIcon icon={['fas', 'leaf']} />
+            </div>
+            <span className={Style.valueSpan}>
+              Comprometimento com o meio ambiente através de projetos de menor impacto.{' '}
+            </span>
+          </div>
+
+          <div className={Style.value}>
+            <div className={Style.icon}>
+              <FontAwesomeIcon icon={['fas', 'handshake']} />
+            </div>
+            <span className={Style.valueSpan}>
+              Comunicação eficiente entre a empresa e os clientes para os alinhamentos dos projetos.{' '}
+            </span>
+          </div>
+
+          <div className={Style.value}>
+            <div className={Style.icon}>
+              <FontAwesomeIcon icon={['fas', 'hard-hat']} />
+            </div>
+            <span className={Style.valueSpan}>
+              Profissionais qualificados e preparados com as melhores técnicas do mercado.{' '}
+            </span>
+          </div>
         </div>
 
-        <div className={Style.value}>
-          <div className={Style.icon}>
-            <FontAwesomeIcon icon={['fas', 'leaf']} />
-          </div>
-          <span>Comprometimento com o meio ambiente através de projetos de menor impacto. </span>
-        </div>
-
-        <div className={Style.value}>
-          <div className={Style.icon}>
-            <FontAwesomeIcon icon={['fas', 'handshake']} />
-          </div>
-          <span>Comunicação eficiente entre a empresa e os clientes para os alinhamentos dos projetos. </span>
-        </div>
-
-        <div className={Style.value}>
-          <div className={Style.icon}>
-            <FontAwesomeIcon icon={['fas', 'hard-hat']} />
-          </div>
-          <span>Profissionais qualificados e preparados com as melhores técnicas do mercado. </span>
+        <div className={Style.quemSomos}>
+          <h2 className={Util.titleBlue}>Quem somos</h2>
+          <p className={Style.content}>
+            A Pluris Engenharia é um empresa formada por profissionais qualificados e comprometidos em entregar sempre o
+            melhor aos seus clientes. Atuando há 3 anos no mercado, a Pluris Engenharia mantém como objetivo realizar os
+            sonhos dos seus clientes seguindo os valores e princípios da empresa.{' '}
+          </p>{' '}
+          <button className={Util.buttonBlue}>Venha nos conhecer melhor</button>
         </div>
       </div>
-
-      <div className={Style.quemSomos}>
-        <h2 className={utilStyles.titleBlack}>Quem somos</h2>
-        <p className={Style.content}>
-          A Pluris Engenharia é um empresa formada por profissionais qualificados e comprometidos em entregar sempre o
-          melhor aos seus clientes. Atuando há 3 anos no mercado, a Pluris Engenharia mantém como objetivo realizar os
-          sonhos dos seus clientes seguindo os valores e princípios da empresa.{' '}
-        </p>{' '}
-        <button className={utilStyles.buttonBlue}>Venha nos conhecer melhor</button>
+      <div className={Util.blackSpace}>
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1000 100" preserveAspectRatio="none">
+          <path className="elementor-shape-fill" d="M761.9,44.1L643.1,27.2L333.8,98L0,3.8V0l1000,0v3.9"></path>
+        </svg>
       </div>
-    </div>
+    </>
   )
 }
--- pluris-page/pages/index.js
+++ pluris-page/pages/index.js
@@ -2,6 +2,7 @@ import Layout from '../components/layout'
 import Inicio from '../components/Home/Home'
 import About from '../components/About'
 import Servicos from '../components/Servicos'
+import Util from '../styles/util.module.scss'
 
 const name = 'home foto'
 const siteTitle = 'Início'
