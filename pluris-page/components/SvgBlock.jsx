9083045efcb24bd239828a7a93460363980b7db6
Amanda Carneiro <amandabcarneiro9@gmail.com>
2021-09-22 14:13:38 +0200

    svg as a component, and opposite

--- pluris-page/components/SvgBlock.jsx
+++ pluris-page/components/SvgBlock.jsx
@@ -1,10 +1,10 @@
 import Style from './svgBlock.module.scss'
 
-export function SvgBlock({ classNameColor, opposite }) {
+export function SvgBlock({ svgClassNameColor, opposite }) {
   const svgClassName = opposite ? Style.svgOpposite : ''
   return (
     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1000 100" preserveAspectRatio="none" className={svgClassName}>
-      <path className={classNameColor} d="M761.9,44.1L643.1,27.2L333.8,98L0,3.8V0l1000,0v3.9"></path>
+      <path className={svgClassNameColor} d="M761.9,44.1L643.1,27.2L333.8,98L0,3.8V0l1000,0v3.9"></path>
     </svg>
   )
 }
