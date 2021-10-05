fc7c7c2964f4a19a1df621128007f8cc43034a5f
Amanda Carneiro <amandabcarneiro9@gmail.com>
2021-10-05 16:15:27 +0200

    made contact page responsive e prettier

--- pluris-page/pages/contato.js
+++ pluris-page/pages/contato.js
@@ -14,27 +14,36 @@ export default function Contato() {
         <div className={Style.content}>
           <div className={Style.informations}>
             <span className={Style.firstMessage}>
-              Tem sugestões, duvidas ou gostaria de um orçamento ? Entre em contato através do formulário ao lado.
+              Tem sugestões, dúvidas ou gostaria de um orçamento ? Entre em contato através do nosso formulário .
             </span>
             <span>Ficaremos feliz em te ajudar !</span>
-            <span className={Style.secondMessage}>Você também pode nos encontrar por aqui :</span>
-            <div className={Style.contactIcons}>
-              <div className={Style.contacts}>
-                <FontAwesomeIcon icon={['fab', 'whatsapp']} className={Style.icon} />
-                <span className={Style.info}>(xx)xxxx-xxxxx</span>
-              </div>
-              <div className={Style.contacts}>
-                <FontAwesomeIcon icon={['fas', 'envelope-square']} className={Style.icon} />
-                <span className={Style.info}>comercial@plurisengenharia.com</span>
-              </div>
+          </div>
+          <div className={Style.contactIcons}>
+            <span className={util.titleBlueMinor}>Você também pode nos encontrar por aqui </span>
+            <div className={Style.contacts}>
+              <FontAwesomeIcon icon={['fab', 'whatsapp']} className={Style.icon} />
+              <span className={Style.info}>(xx)xxxx-xxxxx</span>
+            </div>
+            <div className={Style.contacts}>
+              <FontAwesomeIcon icon={['fas', 'envelope-square']} className={Style.icon} />
+              <span className={Style.info}>comercial@plurisengenharia.com</span>
+            </div>
 
-              <div className={Style.contacts}>
-                <FontAwesomeIcon icon={['fas', 'map-marker-alt']} className={Style.icon} />
-                <span className={Style.info}>Rua: exemplo exemplo exemplo,980 Bairro Cidade</span>
-              </div>
+            <div className={Style.contacts}>
+              <FontAwesomeIcon icon={['fas', 'map-marker-alt']} className={Style.icon} />
+              <span className={Style.info}>Rua: exemplo exemplo exemplo,980 Bairro Cidade</span>
+            </div>
+
+            <div className={Style.contacts}>
+              <a href="https://www.instagram.com/pluris_engenharia/">
+                <FontAwesomeIcon icon={['fab', 'instagram-square']} className={Style.icon} />
+              </a>
+              <span className={Style.info}>@pluris_engenharia</span>
             </div>
           </div>
-          <ContactForm />
+          <div className={Style.contactForm}>
+            <ContactForm />
+          </div>
         </div>
       </div>
     </Layout>
